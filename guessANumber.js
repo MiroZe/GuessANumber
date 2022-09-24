d667edcebba3b736006166c6aa281e5d0f4e4c2f
MiroZe <114018004+MiroZe@users.noreply.github.com>
2022-09-24 09:24:23 +0300

    add js file

---
+++ guessANumber.js
@@ -0,0 +1,13 @@
+const readline = require('readline').createInterface({
+    iput: process.stdin,
+    output: process.stdout
+});
+
+let computerGuess = Math.floor(Math.random() * 100);
+let recursiveAsyncReadline = function () {
+    readline.question('Guess the number (0-100): ', number =>
+{
+
+});
+}
+recursiveAsyncReadline();
