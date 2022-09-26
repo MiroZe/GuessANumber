7a6161b7924e9fc1275a3db0accc071555e8ba62
MiroZe <miro_sv@yahoo.com>
2022-09-26 14:38:54 +0300

    Update guessANumber.js

--- guessANumber.js
+++ guessANumber.js
@@ -1,13 +1,34 @@
-const readline = require('readline').createInterface({
-    iput: process.stdin,
-    output: process.stdout
-});
 
-let computerGuess = Math.floor(Math.random() * 100);
-let recursiveAsyncReadline = function () {
-    readline.question('Guess the number (0-100): ', number =>
-{
+function guessANumber () {
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
 
-});
+    let computerGuess = Math.floor(Math.random() * 100);
+    let guess;
+
+    let recursiveAsyncReadline = function () {
+        readline.question('Guess the number (0-100): ', number => {
+            guess = Number(number);
+            if (guess <= 100 && guess >=0 ) {
+                if (guess === computerGuess) {
+                    console.log('You guess it');
+                    return readline.close()
+                } else if( guess < computerGuess ) {
+                            console.log('Too Low!');
+                            return recursiveAsyncReadline();
+                } else if( guess > computerGuess) {
+                            console.log('Too High!');
+                            return recursiveAsyncReadline()
+                } else {
+                            console.log('Invalid input! Try again...');
+                            recursiveAsyncReadline()
+                }
+            }
+        });
+    }
+    recursiveAsyncReadline();
+
 }
-recursiveAsyncReadline();
+guessANumber()
